14c15c759831f69c2549d7fc23f1b9ab48f802e7
Alondra <alonrodriguez@csumb.edu>
2023-11-20 15:46:14 -0800

    Added filters page to add filter on image

--- flaskFront/cstProject.py
+++ flaskFront/cstProject.py
@@ -1,19 +1,27 @@
-# impliment filters here
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
-from flask import url_for
 
-# create an instance of Flask
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
-# route decorator binds a function to a URL
 @app.route('/')
 def index():
-    def filters():
-        p1 = '<p>waiting for filters</p>'
     return render_template("index.html", image_url=url_for('static', filename='images/tree.jpg'))
+
+@app.route('/filters', methods=['POST'])
+def filters():
+    if request.form['filter'] == 'Generate Pictures':
+        # Implement your logic to generate pictures
+        pass
+    elif request.form['filter'] == 'Apply Filter':
+        # Redirect to the filter selection page
+        return redirect(url_for('filter_selection'))
+
+    return redirect(url_for('index'))
+
+@app.route('/filter_selection')
+def filter_selection():
+    return render_template("filter_selection.html", image_url=url_for('static', filename='images/tree.jpg'))
 
 if __name__ == '__main__':
     app.run(debug=True)
