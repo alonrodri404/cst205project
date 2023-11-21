7332241ed47c50c746ede6d934215181394d8774
Alondra <alonrodriguez@csumb.edu>
2023-11-20 16:13:33 -0800

    html test

--- flaskFront/backend.py
+++ flaskFront/backend.py
@@ -1,40 +1,32 @@
 import requests
 import os
+from flask import url_for
  
 def generate_animal_pictures():
-    """
-    Function to generate 100 animal pictures.
- 
-    This function uses the Unsplash API to fetch random animal pictures.
-    It saves the pictures in a local directory named 'animal_pictures'.
- 
-    Returns:
-    - None
- 
-    Raises:
-    - requests.exceptions.RequestException:
-        If there is an error while making the API request.
-    """
- 
+    image_urls = []
+    image_dir = 'static/images'  # Define the directory path
+
     # Create a directory to store the animal pictures if it doesn't exist
-    if not os.path.exists('animal_pictures'):
-        os.makedirs('animal_pictures')
- 
-    # Fetch 100 random animal pictures from the Unsplash API
-    for i in range(100):
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
+
+    # Fetch and save a certain number of random animal pictures
+    for i in range(1):  # Reduced the number for demonstration purposes
         try:
             response = requests.get('https://source.unsplash.com/random/500x500', stream=True)
             response.raise_for_status()
- 
-            # Save the picture to the local directory
-            with open(f'animal_pictures/animal_{i+1}.jpg', 'wb') as file:
+
+            # Define the image path
+            image_path = f'{image_dir}/animal_{i+1}.jpg'
+            with open(image_path, 'wb') as file:
                 for chunk in response.iter_content(chunk_size=1024):
                     file.write(chunk)
- 
-            print(f"Animal picture {i+1} saved successfully.")
- 
+            
+            # Generate the URL for the saved image
+            image_urls.append(url_for('static', filename=f'images/animal_{i+1}.jpg'))
+
         except requests.exceptions.RequestException as e:
             print(f"Error while fetching animal picture {i+1}: {e}")
- 
-# Call the function to generate the animal pictures
-generate_animal_pictures()
+
+    return image_urls
+
--- flaskFront/cstProject.py
+++ flaskFront/cstProject.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
+from backend import generate_animal_pictures
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -11,13 +13,14 @@ def index():
 @app.route('/filters', methods=['POST'])
 def filters():
     if request.form['filter'] == 'Generate Pictures':
-        # Implement your logic to generate pictures
-        pass
+        image_urls = generate_animal_pictures()  # This will return a list of image URLs
+        return render_template("index.html", image_urls=image_urls)
     elif request.form['filter'] == 'Apply Filter':
         # Redirect to the filter selection page
         return redirect(url_for('filter_selection'))
 
     return redirect(url_for('index'))
+
 
 @app.route('/filter_selection')
 def filter_selection():
