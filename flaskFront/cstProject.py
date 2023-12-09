d4dab296ccd0f05c5fd59a48e702b5a6fbb905f4
Alondra <alonrodriguez@csumb.edu>
2023-12-09 11:27:43 -0800

    made upload work

--- flaskFront/cstProject.py
+++ flaskFront/cstProject.py
@@ -1,14 +1,53 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from backend import generate_animal_pictures
+import os
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = 'static/images'  # Set your upload folder
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            save_path = os.path.join(app.config['UPLOAD_FOLDER'], 'animal_1.jpg')
+            file.save(save_path)
+            return redirect(url_for('index'))
+
+    return render_template("upload.html")
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html", image_url=url_for('static', filename='images/tree.jpg'))
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            save_path = os.path.join(app.config['UPLOAD_FOLDER'], 'animal_1.jpg')
+            file.save(save_path)
+            return redirect(url_for('index'))
+
+    image_url = url_for('static', filename='images/animal_1.jpg') if os.path.exists('static/images/animal_1.jpg') else None
+    return render_template("index.html", image_url=image_url)
 
 @app.route('/filters', methods=['POST'])
 def filters():
